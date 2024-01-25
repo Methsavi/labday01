0aee921b57a897c6b8c3f250662ab5083d61dd78
Methsavi <hmmsavindya.students.nsbm.ac.lk>
2024-01-25 11:42:33 +0530

    lab01q3 done

---
+++ lab01q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0;
+            Console.Write(" Enter a positive number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    sum = sum + i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
