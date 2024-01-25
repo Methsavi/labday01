120005e9e371da46f4b05cb3ab62c2f8fc51277a
Methsavi <hmmsavindya.students.nsbm.ac.lk>
2024-01-25 11:35:14 +0530

    Add project files.

---
+++ lapday01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lapday01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter lenth: ");
+            double lenth = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = Area(lenth, width);
+
+            Console.WriteLine("Your Area is: " + area);
+            Console.ReadLine();
+
+        }
+        static double Area(double lenth, double width)
+        {
+            return lenth * width;
+        }
+    }
+    
+}
